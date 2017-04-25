b42690adf85b3f39a2813fd6829f90a07e5e5c66
Elizabeth Moran <moran19@up.edu>
2017-04-25 11:17:41 -0700

    Tests work now

--- app/src/test/java/com/example/ryanmar19/quarto/QuartoStateTest.java
+++ app/src/test/java/com/example/ryanmar19/quarto/QuartoStateTest.java
@@ -7,7 +7,9 @@ import com.example.ryanmar19.quarto.game.infoMsg.GameInfo;
 import com.example.ryanmar19.quarto.game.infoMsg.GameState;
 import com.example.ryanmar19.quarto.quarto.Piece;
 import com.example.ryanmar19.quarto.quarto.QuartoClaimVictoryAction;
+import com.example.ryanmar19.quarto.quarto.QuartoHumanPlayer;
 import com.example.ryanmar19.quarto.quarto.QuartoPickPieceAction;
+import com.example.ryanmar19.quarto.quarto.QuartoPlayPieceAction;
 import com.example.ryanmar19.quarto.quarto.QuartoState;
 
 import org.junit.Test;
@@ -16,107 +18,47 @@ import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
 
 public class QuartoStateTest {
-/*
-    @Test
-    public void testPickPieceAction() throws Exception {
-        *//*QuartoState state = new QuartoState();
-        QuartoPickPieceAction action = new QuartoPickPieceAction();
-        action.pickedPiece = state.pieces[0];
-        state.PickPieceAction(action);
-        boolean blah = state.pickedPiece.equals(state.pieces[0]);
-        *//*
-    }
 
     @Test
-    public void testPlacePieceAction() throws Exception {
-
-    }
-
-    @Test
-    public void testClaimVictoryAction() throws Exception {
+    public void testQuartoPickPieceAction() throws Exception {
         QuartoState state = new QuartoState();
-        QuartoClaimVictoryAction action = new QuartoClaimVictoryAction();
-
-        state.setBoardPiece(state.pieces[0],0,0);
-        state.setBoardPiece(state.pieces[1],0,1);
-        state.setBoardPiece(state.pieces[2],0,2);
-        state.setBoardPiece(state.pieces[3],0,3);
-        boolean blah = state.ClaimVictoryAction(action);
+        GamePlayer Maggie = new QuartoHumanPlayer("Maggie");
+        QuartoPickPieceAction action = new QuartoPickPieceAction(Maggie, 3);
+        state.PickPieceAction(action);
+        boolean blah = state.pickedPiece.equals(state.bankPieces[3]);
         assertTrue(blah);
     }
 
     @Test
-    public void testGetPlayerId() throws Exception {
+    public void testQuartoPlayPieceAction() throws Exception {
         QuartoState state = new QuartoState();
+        GamePlayer Lucy = new QuartoHumanPlayer("Lucy");
 
+        QuartoPickPieceAction action = new QuartoPickPieceAction(Lucy, 1);
+        state.PickPieceAction(action);
+
+        QuartoPlayPieceAction action2 = new QuartoPlayPieceAction(Lucy, 2, 2, 1);
+        state.PlayPieceAction(action2);
+
+        boolean blah = state.boardPieces[2][2].equals(state.pieceLib[1]);
+        assertTrue(blah);
     }
 
-    @Test
-    public void setPlayerId() throws Exception {
-
-    }
 
     @Test
-    public void getTurn() throws Exception {
-
-    }
-
-    @Test
-    public void changeTurn() throws Exception {
-
-    }
-
-    @Test
-    public void testGetBankPiece() throws Exception {
+    public void testQuartoClaimVictoryAction() throws Exception {
         QuartoState state = new QuartoState();
-        Piece piece1 = state.getBankPiece(0);
-        Piece piece2 = state.pieces[0];
+        GamePlayer Liz = new QuartoHumanPlayer("Liz");
 
-        assertEquals(piece1, piece2);
+        state.boardPieces[0][0] = state.pieceLib[1];
+        state.boardPieces[1][1] = state.pieceLib[2];
+        state.boardPieces[2][2] = state.pieceLib[3];
+        state.boardPieces[3][3] = state.pieceLib[4];
+
+        QuartoClaimVictoryAction action = new QuartoClaimVictoryAction(Liz);
+        state.ClaimVictoryAction(action);
+
+        boolean blah = state.checkIfQuarto();
+        assertTrue(blah);
     }
-
-    @Test
-    public void testGetBoardPiece() throws Exception {
-        QuartoState state = new QuartoState();
-        state.boardPieces[0][0] = state.pieces[0];
-        Piece piece = state.getBoardPiece(0,0);
-        assertEquals(piece, state.pieces[0]);
-    }
-
-    @Test
-    public void setBoardPiece() throws Exception {
-        QuartoState state = new QuartoState();
-        state.setBoardPiece(state.pieces[0],0,0);
-        assertEquals(state.boardPieces[0][0], state.pieces[0]);
-    }
-
-    @Test
-    public void testGetPieceId() throws Exception {
-        QuartoState state = new QuartoState();
-        int num = state.getPieceId(state.pieces[0]);
-        assertEquals(num,0);
-    }
-
-    @Test
-    public void getPieceColor() throws Exception {
-        QuartoState state = new QuartoState();
-        char c = state.getPieceColor(state.pieces[0]);
-        assertEquals('b',c);
-    }
-
-    @Test
-    public void getPieceSize() throws Exception {
-
-    }
-
-    @Test
-    public void getPieceShape() throws Exception {
-
-    }
-
-    @Test
-    public void getPieceSolidity() throws Exception {
-
-    }*/
-
 }
